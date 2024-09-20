b7dbbf3fcd595af6c1319c3c5e9723f80a54211a
Manav Malhotra <manavmalhotrafrnd4u@gmail.com>
2024-09-21 00:47:08 +0530

    test route

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,43 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const notesRouter = require('./controllers/notes')
-const userRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
-const cors = require('cors')
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const notesRouter = require("./controllers/notes");
+const userRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const cors = require("cors");
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
-console.log('connecting to', config.MONGODB_URI)
+console.log("connecting to", config.MONGODB_URI);
 
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    console.log('connected to MongoDB')
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.error('error connection to MongoDB:', error.message)
-  })
+    console.error("error connection to MongoDB:", error.message);
+  });
 
-app.use(cors())
-app.options('*', cors())
+app.use(cors());
+app.options("*", cors());
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/notes', notesRouter)
-app.use('/api/user', userRouter)
-app.use('/api/login', loginRouter)
+app.get("/", (req, res) => {
+  return {
+    msg: "Hello World",
+  };
+});
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api/notes", notesRouter);
+app.use("/api/user", userRouter);
+app.use("/api/login", loginRouter);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // app.use((req, res, next) => {
 // 	res.header('Access-Control-Allow-Origin', '*');
@@ -39,7 +45,6 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 // 	next();
 //   });
-  
 
 // if (process.env.NODE_ENV === 'test') {
 //   const testingRouter = require('./controllers/testing')
@@ -49,4 +54,6 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // app.use(middleware.unknownEndpoint)
 // app.use(middleware.errorHandler)
 
-app.listen(config.PORT,()=>{`Listening on ${config.PORT}`})
+app.listen(config.PORT, () => {
+  `Listening on ${config.PORT}`;
+});
