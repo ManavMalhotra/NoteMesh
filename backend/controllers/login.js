bb2ee53470d5144f4a5b9335f63cb7f21d21c2b7
Manav Malhotra <manavmalhotrafrnd4u@gmail.com>
2024-09-26 18:14:32 +0530

    improved authentication logic

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,38 +1,40 @@
-const loginRouter = require('express').Router()
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-const Note = require('../models/note')
-const User = require('../models/user')
+const loginRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
-loginRouter.post('/',async (req,res)=>{
-	const {username,password} = req.body
+loginRouter.post('/', async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-	const user = await User.findOne({username})
-	const passwordCorrect = user === null ? false : bcrypt.compare(password,user.passwordHash)
+    // Check if username exists
+    const user = await User.findOne({ username });
+    const passwordCorrect = user === null ? false : await bcrypt.compare(password, user.passwordHash);
 
-	if (!(user && passwordCorrect)) {
-		return res.status(401).json({
-			error: "invalid username or password"
-		})
-	}
+    if (!(user && passwordCorrect)) {
+      return res.status(401).json({
+        error: "invalid username or password"
+      });
+    }
 
-	const userToken = {
-		username: user.username,
-		id: user._id
-	}
+    // Create JWT token
+    const userToken = {
+      username: user.username,
+      id: user._id
+    };
 
-	const token = jwt.sign(userToken,process.env.SECRET)
+    const token = jwt.sign(userToken, process.env.SECRET, { expiresIn: '1h' }); // Token valid for 1 hour
 
-	console.log("TOKEN: ", token)
+    // Send token and user data
+    res.status(200).json({
+      token,
+      username: user.username,
+      name: user.name
+    });
 
-	res
-	.status(200)
-	.send({
-		token,
-		username:user.username,
-		name:user.name
-		})
+  } catch (error) {
+    res.status(500).json({ error: 'something went wrong' });
+  }
+});
 
-})
-
-module.exports = loginRouter
+module.exports = loginRouter;
